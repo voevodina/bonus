ef903d33c68c30f48001d2f973cce5006af3d091
voevodina <voevodina_lyubov@mail.ru>
2020-03-30 10:41:28 +0300

    first commit

---
+++ Main128000.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        //amount - сумма пополнения
+        // balance - текущий баланс клиента
+        long balance = 100;
+        long bonus;
+        long amount = 128000;
+        if (amount > 1000) {
+            bonus = (balance + amount) / 100;
+        } else {
+            bonus = 0;
+        }
+        balance = balance + amount + bonus;
+        System.out.println(bonus);
+    }
+}
